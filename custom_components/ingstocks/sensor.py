0aaf1d16c16970a450f20f02033b3c65f903c771
RxGvQCxU <ralf@essbaumer.de>
2025-11-28 12:39:32 +0100

    Add key figures to INGStocks sensor attributes
    The sensor now fetches additional key figures (dividend yield, dividend per share, P/E ratio, market capitalization, 52-week high/low, etc.) from a second API endpoint and includes them in the sensor attributes. Version bumped to 1.0.4 in manifest.

--- custom_components/ingstocks/sensor.py
+++ custom_components/ingstocks/sensor.py
@@ -1,9 +1,9 @@
+
 import logging
 import aiohttp
 from homeassistant.helpers.entity import Entity
 
 _LOGGER = logging.getLogger(__name__)
-
 DOMAIN = "ingstocks"
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
@@ -44,28 +44,49 @@ class INGStocksSensor(Entity):
         return self._attributes
 
     async def async_update(self):
-        url = f"https://component-api.wertpapiere.ing.de/api/v1/components/instrumentheader/{self._isin}"
-        _LOGGER.debug("Rufe API auf: %s", url)
+        header_url = f"https://component-api.wertpapiere.ing.de/api/v1/components/instrumentheader/{self._isin}"
+        keyfigures_url = f"https://component-api.wertpapiere.ing.de/api/v1/share-ng/keyfigures/{self._isin}"
+        _LOGGER.debug("Rufe API auf: %s", header_url)
+
         try:
             async with aiohttp.ClientSession() as session:
-                async with session.get(url) as response:
+                # Erster Request: Basisdaten
+                async with session.get(header_url) as response:
                     data = await response.json()
-                    _LOGGER.debug("API Antwort: %s", data)
-                    price = data.get("price")
-                    if price is None:
-                        _LOGGER.warning("Kein Preis für ISIN %s gefunden!", self._isin)
-                        self._state = "unavailable"
-                        return
-                    self._state = round(price, 2)
-                    self._attributes = {
-                        "name": data.get("name"),
-                        "isin": data.get("isin"),
-                        "currency": data.get("currencySign"),
-                        "change_percent": round(data.get("changePercent", 0), 2),
-                        "change_absolute": data.get("changeAbsolute"),
-                        "exchange": data.get("exchangeName"),
-                        "last_update": data.get("priceChangeDate"),
-                    }
+                _LOGGER.debug("API Antwort instrumentheader: %s", data)
+
+                price = data.get("price")
+                if price is None:
+                    _LOGGER.warning("Kein Preis für ISIN %s gefunden!", self._isin)
+                    self._state = "unavailable"
+                    return
+
+                self._state = round(price, 2)
+                self._attributes = {
+                    "name": data.get("name"),
+                    "isin": data.get("isin"),
+                    "currency": data.get("currencySign"),
+                    "change_percent": round(data.get("changePercent", 0), 2),
+                    "change_absolute": data.get("changeAbsolute"),
+                    "exchange": data.get("exchangeName"),
+                    "last_update": data.get("priceChangeDate"),
+                }
+
+                # Zweiter Request: Kennzahlen
+                async with session.get(keyfigures_url) as response:
+                    keyfigures = await response.json()
+                _LOGGER.debug("API Antwort keyfigures: %s", keyfigures)
+
+                self._attributes.update({
+                    "dividend_yield": round(keyfigures.get("dividendYield", 0), 4),
+                    "dividend_per_share": keyfigures.get("dividendPerShare"),
+                    "price_earnings_ratio": keyfigures.get("priceEarningsRatio"),
+                    "market_capitalization": keyfigures.get("marketCapitalization"),
+                    "market_cap_currency": keyfigures.get("marketCapitalizationCurrencyIsoCode"),
+                    "52w_low": keyfigures.get("fiftyTwoWeekLow"),
+                    "52w_high": keyfigures.get("fiftyTwoWeekHigh"),
+                })
+
         except Exception as e:
             _LOGGER.error("Fehler beim Abrufen der Aktie %s: %s", self._isin, e)
             self._state = "unavailable"
